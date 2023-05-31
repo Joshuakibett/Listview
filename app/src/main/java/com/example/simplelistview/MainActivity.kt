6706f8707da39036651bbea6b1e196d6b23556a6
joshuakibett <joshuaulempoa772@gmail.com>
2023-05-31 16:50:45 +0300

    initial commit

---
+++ app/src/main/java/com/example/simplelistview/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.simplelistview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val listviewyangu=findViewById<ListView>(R.id.mylistview)
+        val nchi= arrayOf("Kenya","Tanzania","Uganda","Burundi","Rwanda")
+        val arrayAdapter:ArrayAdapter<String> =ArrayAdapter(this,android.R.layout.simple_list_item_1,nchi)
+        listviewyangu.adapter=arrayAdapter
+
+        listviewyangu.setOnItemClickListener { parent, view, position, id ->
+            Toast.makeText(this,"You have clicked on"+nchi[position],Toast.LENGTH_LONG).show()
+        }
+          
+    }
+}
